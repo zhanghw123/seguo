a03084c4d65fba3e578e1b9ec35fd5b39bf51b68
zhanghw <18831215676@163.com>
2019-11-05 09:51:17 +0800

    123

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import { version, Button } from "antd";
+import "antd/dist/antd.css";
 
-ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(
+  <div className="App">
+    <h1>Please fork this codesandbox to reproduce your issue.</h1>
+    <div>Current antd version: {version}</div>
+    <div style={{ marginTop: "16px" }}>
+      <Button type="primary">Example button</Button>
+    </div>
+  </div>,
+  document.getElementById("root")
+);
